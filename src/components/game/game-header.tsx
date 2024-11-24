6a8306c1ed0c58c4bf9c865d0b3859d774662e91
Aviral Sharma <thisisavi200@gmail.com>
2024-11-24 20:23:19 +0530

    Update game page components

--- src/components/game/game-header.tsx
+++ src/components/game/game-header.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const GameHeader = () => {
   return (
-    <header className="h-20 flex justify-between px-5 items-center bg-white bg-opacity-35 backdrop-blur-md shadow-md">
-      <button className="bg-white rounded-2xl p-2">
+    <header className="h-20 flex justify-between px-5 items-center bg-white bg-opacity-35 backdrop-blur-md shadow-sm">
+      <button className="bg-transparent">
         <Home className="text-black" />
       </button>
-      <div className="bg-white text-2xl font-pencil font-bold rounded-3xl px-8 py-2">Your Turn</div>
+      <div className="bg-white text-2xl text-black font-bold rounded-3xl px-8 py-2">Your Turn</div>
       <div className="flex gap-5">
-        <button className="bg-white rounded-2xl p-2">
+        <button className="bg-transparent">
           <Volume2 className="text-black" />
         </button>
-        <button className="bg-white rounded-2xl p-2">
+        <button className="bg-transparent">
           <UserRound className="text-black" />
         </button>
       </div>
